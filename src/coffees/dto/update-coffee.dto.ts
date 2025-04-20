ff00f00f6ab7fb4bb590a32893484b58017f39cd
moelzanaty3 <mohammedelzanaty129@gmail.com>
2025-04-20 20:22:29 +0200

    refactor(coffees): simplify DTO imports and create new DTO files

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
-import { CreateCoffeeDto } from '../create-coffee.dto/create-coffee.dto';
+import { CreateCoffeeDto } from '../create-coffee.dto';
 
 export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
