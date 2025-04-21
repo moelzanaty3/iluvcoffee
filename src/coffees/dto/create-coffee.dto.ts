443063b4e96ac8702a5f0d81a4e127d8682616ac
moelzanaty3 <mohammedelzanaty129@gmail.com>
2025-04-21 22:09:11 +0200

    feat(coffees): add HTTP requests for coffee operations and validation rules

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,5 +1,17 @@
+import { IsNotEmpty, IsOptional, IsString, Length } from 'class-validator';
+
 export class CreateCoffeeDto {
-  name: string;
-  brand: string;
-  flavors: string[];
+  @IsString()
+  @IsNotEmpty()
+  @Length(5, 255)
+  readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 255)
+  readonly brand: string;
+
+  @IsString({ each: true })
+  @IsOptional()
+  readonly flavors: string[];
 }
