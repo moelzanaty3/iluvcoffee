f96edebdf78b000261aa1ef52a473b59a09b13c8
moelzanaty3 <mohammedelzanaty129@gmail.com>
2025-04-27 01:02:12 +0300

    feat(config): add Joi validation schema for environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as Joi from 'joi';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,7 +9,17 @@ import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      cache: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        MONGODB_PORT: Joi.number().port().default(27017),
+        MONGODB_DATABASE: Joi.string().required(),
+        MONGODB_URI: Joi.string().required(),
+      }),
+    }),
     CoffeesModule,
     MongooseModule.forRoot(process.env.MONGODB_URI ?? ''),
   ],
